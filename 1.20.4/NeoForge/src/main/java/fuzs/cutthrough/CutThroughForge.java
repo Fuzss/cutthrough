9de983af90b6a668e61057370a8d5243d7fbe7cf
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-02 13:32:34 +0100

    prepare 1.20.4 port

---
+++ 1.20.4/NeoForge/src/main/java/fuzs/cutthrough/CutThroughForge.java
@@ -0,0 +1,16 @@
+package fuzs.cutthrough;
+
+import fuzs.puzzleslib.api.core.v1.ModConstructor;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+
+@Mod(CutThrough.MOD_ID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CutThroughForge {
+
+    @SubscribeEvent
+    public static void onConstructMod(final FMLConstructModEvent evt) {
+        ModConstructor.construct(CutThrough.MOD_ID, CutThrough::new);
+    }
+}
